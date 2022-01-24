f1fd199ce70bc9b06104469b59b69f60c8e17512
TarrantHightopp <56646924+ritik-chauhan@users.noreply.github.com>
2022-01-24 05:34:47 +0530

    correct BS

--- src/com/ritik/Main.java
+++ src/com/ritik/Main.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr ={1,2,3,4,5,6};
-        BubbleSort(arr);
+        int[] arr ={5,4,3,2,1};
+        SelectionSort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -20,6 +20,7 @@ public class Main {
                     int temp=arr[j-1];
                     arr[j-1]=arr[j];
                     arr[j]=temp;
+                    swapped=true;
                 }
             }
             if(!swapped)
@@ -27,6 +28,45 @@ public class Main {
         }
     }
     static void SelectionSort(int[] arr){
-        
+        for(int i=0; i<arr.length; i++){
+            //find max item in remaining array and replace with correct index
+            int last=arr.length-i-1;
+            int maxIndex=0;
+            //loop to find index of maximum element
+            for (int j=0; j<=last; j++){
+                if(arr[j]>arr[maxIndex]){
+                    maxIndex=j;
+                }
+            }
+            //swap element at maxIndex with element at last
+            int temp=arr[last];
+            arr[last]=arr[maxIndex];
+            arr[maxIndex]=temp;
+        }
+    }
+    static void SelectionSortMod(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            int last = arr.length -i -1;
+            int maxIndex = findMaxIndex(arr, 0, last);
+            swap(arr[i], arr[last]);
+        }
+    }
+
+    private static void swap(int first, int second) {
+        int temp = first;
+        first=second;
+        second=temp;
+
+
+    }
+
+    private static int findMaxIndex(int[] arr, int first, int last) {
+        int maxIndex=first;
+        for(int i=0; i<=last; i++){
+            if(arr[i]<arr[maxIndex]){
+                maxIndex=i;
+            }
+        }
+        return maxIndex;
     }
 }
